f0af24b23b61bba80beb45773d3c3b10b369b3f1
Brayn <ilogirb23@yahoo.com>
2018-10-21 20:06:34 +0800

    running server 2

--- pages/post.js
+++ pages/post.js
@@ -6,11 +6,13 @@ const Post = props => (
 	<Layout>
 		<Head>
 			<title>MyComic | {props.show.name}</title>
-			<meta
-				name="description"
-				content={props.show.summary.replace(/<\/?[^>]+(>|$)/g, "")}
-			/>
 			<meta name="keywords" content="test keywords" />
+			<meta property="og:title" content={props.show.name} />
+			<meta property="og:image" content={props.show.image.medium} />
+			<meta
+				property="og:url"
+				content={`https://api.tvmaze.com/shows/${props.show.id}`}
+			/>
 		</Head>
 
 		<h1>{props.show.name}</h1>
